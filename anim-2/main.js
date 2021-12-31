75958173b882a056bb2ff07377c022e17ccc1c88
sriram005678 <sriram005678@gmail.com>
2021-12-31 20:48:03 +0530

    process-3

--- anim-2/main.js
+++ anim-2/main.js
@@ -7,34 +7,36 @@ function startAnim(){
     document.getElementById("bottomline").style.transition = 'all 0.5s ease';
     },500);
 
-    setTimeout(function(){
-    document.getElementById("logo").style.transform = 'translateX(0px)';
-    document.getElementById("logo").style.transition = 'all 0.5s ease-out';
-    },1000);
+    
 
     setTimeout(function(){
     document.getElementById("text-1").style.transform = 'translateX(0px)';
     document.getElementById("text-1").style.transition = 'all 0.5s ease-out';
-    },1500);
+    },1000);
 
     setTimeout(function(){
     document.getElementById("text-2").style.transform = 'translateX(0px)';
     document.getElementById("text-2").style.transition = 'all 0.5s ease-out';
-    },2000);
+    },1500);
 
     setTimeout(function(){
     document.getElementById("text-3").style.transform = 'translateY(0px)';
     document.getElementById("text-3").style.transition = 'all 0.5s ease-out';
-    },2500);
+    },2000);
 
     setTimeout(function(){
-    document.getElementById("cta").style.transform = 'translateY(0px)';
+    document.getElementById("cta").style.transform = 'translateX(0px)';
     document.getElementById("cta").style.transition = 'all 0.5s ease-out';
-    },3000);
+    },2500);
 
     setTimeout(function(){
     document.getElementById("url").style.transform = 'translateX(0px)';
     document.getElementById("url").style.transition = 'all 0.5s ease-out';
+    },3000);
+
+    setTimeout(function(){
+    document.getElementById("logo").style.opacity = '1';
+    document.getElementById("logo").style.transition = 'all 0.5s ease-out';
     },3500);
 
     
---
+++ task-3.2-(160 x 600)/main.js
@@ -0,0 +1,38 @@
+function startAnim(){
+
+    var startTime = 0;
+
+    var SM = new TimelineMax();
+
+    startTime += 0.5;
+    SM.to("#textbg", 0.5, { rotateY:0, ease:Power1.easeOut}, startTime);
+    SM.to("#photo", 0.5, { rotateY:0, ease:Power1.easeOut}, startTime);
+
+    startTime += 0.5;
+    SM.to("#logo", 0.5, { scale:1.4, ease:Power2.easeOut}, startTime);
+    startTime += 0.4;
+    SM.to("#logo", 0.5, { scale:1, ease:Power2.easeOut}, startTime);
+
+    startTime += 0.5;
+    SM.to("#text-1", 0.5, { rotateX:0, ease:Power1.easeOut}, startTime);
+
+    startTime += 0.5;
+    SM.to("#text-2", 0.5, { x:0, opacity:1, ease:Power1.easeOut}, startTime);
+
+    startTime += 0.5;
+    SM.to("#text-3", 0.5, { x:0, opacity:1, ease:Power1.easeOut}, startTime);
+
+    startTime += 0.5;
+    SM.to("#text-4", 0.5, { y:0, opacity:1, ease:Power1.easeOut}, startTime);
+
+    startTime += 0.5;
+    SM.to("#circle", 0.5, { rotation:-360, opacity:1, ease:Power1.easeOut}, startTime);
+
+    startTime += 0.5;
+    SM.to("#bottom", 0.5, { scale:1.4, ease:Power2.easeOut}, startTime);
+    startTime += 0.4;
+    SM.to("#bottom", 0.5, { scale:1, ease:Power2.easeOut}, startTime);
+}
+
+
+window.onload = startAnim();
